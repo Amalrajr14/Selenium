8b94f56f0c83cd220f3db35deedbb64e9f3743bf
dell <dell@AmalRaj>
2024-11-13 22:43:15 +0530

    ssd

---
+++ Selenium/src/com/amalraj/selenium/firstProgram/Child2.java
@@ -0,0 +1,23 @@
+package com.amalraj.selenium.firstProgram;
+
+public class Child2 extends Child1 {
+	void print() {
+		
+		System.out.println("Neww");
+	}
+	
+	public static void main(String[] args) {
+		Child2 obj = new Child2();
+
+        
+        obj.display();   
+        obj.show();  
+        obj.print();  
+    }
+	
+	
+}
+	
+	
+
+
