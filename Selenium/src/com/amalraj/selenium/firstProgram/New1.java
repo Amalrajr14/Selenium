1e06cde5a99182ed912734a1b2884ad77bebe3c0
dell <dell@AmalRaj>
2024-11-14 20:43:47 +0530

    aws

---
+++ Selenium/src/com/amalraj/selenium/firstProgram/New1.java
@@ -0,0 +1,17 @@
+package com.amalraj.selenium.firstProgram;
+
+public class New1 extends New {
+	void display() {
+		
+		
+		System.out.println("Newwwww");
+	}
+	
+	public static void main(String[] args) {
+        New obj = new New1();  
+        obj.display();  
+    }
+	
+	
+
+}
